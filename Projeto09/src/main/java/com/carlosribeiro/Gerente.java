0e78cef0db5efd2650df621cd6d625a10eebf64b
luisastellet <luisastellet@id.uff.br>
2024-04-14 15:53:32 -0300

    Projeto 09

---
+++ Projeto09/src/main/java/com/carlosribeiro/Gerente.java
@@ -0,0 +1,20 @@
+package com.carlosribeiro;
+
+public class Gerente ...      <==
+{
+// ==>
+
+// ==>	public Gerente(...)
+	{
+// ==>
+// ==>
+    }
+    
+   	public void setSecretaria(Empregado e) {
+// ==>
+   	}
+
+   	public Empregado getSecretaria() {
+// ==>
+   	}
+}
